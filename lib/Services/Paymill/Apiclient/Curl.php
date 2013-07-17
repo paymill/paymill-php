e102e631d080c2c7012b121684c6f6ec1ab2ee55
Harenkamp <stefan.harenkamp@payintelligent.de>
2013-07-17 14:33:21 +0200

    adjust changes from merge

--- lib/Services/Paymill/Apiclient/Curl.php
+++ lib/Services/Paymill/Apiclient/Curl.php
@@ -77,8 +77,8 @@ class Services_Paymill_Apiclient_Curl implements Services_Paymill_Apiclient_Inte
                 if (isset($this->_responseArray['body']['response_code'])) {
                     $responseCode = $this->_responseArray['body']['response_code'];
                 }
-                if ($responseCode === '' && isset($this->_responseArray['body']['response_code'])) {
-                    $responseCode = $this->_responseArray['body']['response_code'];
+                if ($responseCode === '' && isset($this->_responseArray['body']['data']['response_code'])) {
+                    $responseCode = $this->_responseArray['body']['data']['response_code'];
                 }
 
                 return array("data" => array(

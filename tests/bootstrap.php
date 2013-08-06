b7b55a92935b5157f59db712d67ac17c2caa5a68
Amer Alimanovic <network@amer8.de>
2013-08-07 00:40:15 +0200

    Remove old autoloading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,15 +20,3 @@ if (!defined('API_TEST_KEY') && getenv('API_TEST_KEY'))
     define('API_TEST_KEY', getenv('API_TEST_KEY'));
 
 define('TOKEN', '098f6bcd4621d373cade4e832627b4f6');
-
-/**
- * Define path to application directory
- */
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../lib'));
-
-// Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-        realpath(APPLICATION_PATH),
-        get_include_path(),
-)));

c488a0e8954ae2fe0e7290e9950f4298990f1af5
Harenkamp <stefan.harenkamp@payintelligent.de>
2013-10-04 13:58:51 +0200

    PM-584 : moved autoloader

--- tests/autoloader.php
+++ tests/autoloader.php
@@ -3,7 +3,7 @@
 /**
  * Define path to application directory
  */
-defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/lib/'));
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../lib/'));
 spl_autoload_extensions(".php");
 spl_autoload_register(function($class) {
         $class = str_replace("\\", "/", $class);

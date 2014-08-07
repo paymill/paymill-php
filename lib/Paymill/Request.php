abd8be80250d27f9ae29bb374f4da47a209ad183
Stefan Harenkamp <stefan.harenkamp@payintelligent.de>
2014-08-07 15:22:53 +0200

    adjust version to 3.0.6

--- lib/Paymill/Request.php
+++ lib/Paymill/Request.php
@@ -12,7 +12,7 @@ use Paymill\Services\ResponseHandler;
 
 /**
  * Base
- * @version 3.1.0
+ * @version 3.0.6
  */
 class Request
 {
@@ -35,7 +35,7 @@ class Request
     /**
      * @var string
      */
-    private $_version = "3.1.0";
+    private $_version = "3.0.6";
 
     /**
      * @var string

bb3949aa508ca38b7358f1fa0617f45d167de237
Stefan Harenkamp <stefan.harenkamp@payintelligent.de>
2014-08-07 13:41:36 +0200

    fix version to 4.0.0

--- lib/Paymill/Request.php
+++ lib/Paymill/Request.php
@@ -12,7 +12,7 @@ use Paymill\Services\ResponseHandler;
 
 /**
  * Base
- * @version 3.0.5
+ * @version 4.0.0
  */
 class Request
 {
@@ -35,7 +35,7 @@ class Request
     /**
      * @var string
      */
-    private $_version = "3.0.5";
+    private $_version = "4.0.0";
 
     /**
      * @var string

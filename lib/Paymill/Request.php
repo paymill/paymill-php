9cd209a271edbe253b771e41e1a9df53c2b79a43
enrico.hofmann <enrico.hofmann@paymill.de>
2016-02-26 11:41:46 +0100

    Add Source also 2 Preauthorizations

--- lib/Paymill/Request.php
+++ lib/Paymill/Request.php
@@ -222,8 +222,11 @@ class Request
         $parameter = $model->parameterize($method);
         $serviceResource = $model->getServiceResource() . $model->getId();
 
-        if(is_a($model, '\Paymill\Models\Request\Transaction') && $method === "create") {
-            $source = !array_key_exists('source',$parameter) ?
+        if ((is_a($model, '\Paymill\Models\Request\Transaction')
+                || is_a($model, '\Paymill\Models\Request\Preauthorization'))
+            && $method === "create"
+        ) {
+            $source = !array_key_exists('source', $parameter) ?
                 "PhpLib" . $this->getVersion() :
                 "PhpLib" . $this->getVersion() . "_" . $parameter['source'];
             $parameter['source'] = $source;
